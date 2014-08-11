e7dd360aa6fa1384837b3dbe0cc2c493e0214ef4
egjimenezg <egjimenezg@gmail.com>
2014-08-10 20:15:32 -0500

    coding #1 modifying the controller again

--- Geb/GrailsApp/Converter/grails-app/controllers/com/makingdevs/ConverterController.groovy
+++ Geb/GrailsApp/Converter/grails-app/controllers/com/makingdevs/ConverterController.groovy
@@ -7,17 +7,16 @@ class ConverterController {
   def index(){}
   
   def convert(){
-    def convertedNumber
-    if(params.numericValue){
-      try{
-        convertedNumber = converterService.convertNumber(params.int('type'),Integer.parseInt(params.numericValue)) 
-      }
-      catch(Exception exception){
-        flash.message = 'El número no puede ser convertido'
-      }
-
-      render view:'index',model:[result:convertedNumber]
+    
+    Integer convertedNumber    
+    try{
+      convertedNumber = converterService.convertNumber(params.int('type'),Integer.parseInt(params.numericValue))
     }
-  }       
+    catch(Exception exception){
+      flash.message = 'El número no puede ser convertido'
+    }
+
+    render view:'index',model:[result:convertedNumber]
+  }
   
 }
