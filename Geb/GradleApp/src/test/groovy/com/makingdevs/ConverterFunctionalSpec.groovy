4b77aa701b1340707f64f052b6e24748bf55790a
egjimenezg <egjimenezg@gmail.com>
2014-08-23 13:34:33 -0500

    coding #2 adding new test

--- Geb/GradleApp/src/test/groovy/com/makingdevs/ConverterFunctionalSpec.groovy
+++ Geb/GradleApp/src/test/groovy/com/makingdevs/ConverterFunctionalSpec.groovy
@@ -7,7 +7,6 @@ import geb.spock.*
 
 class ConverterFunctionalSpec extends GebSpec { 
 
-  @Ignore
   def "Conversion of a binary number to decimal"(){
     when:
       to ConverterPage
@@ -19,6 +18,25 @@ class ConverterFunctionalSpec extends GebSpec {
     then: "I should see 2 as the result"
       result.text() == "2"
   }
+  
+  @Unroll("If I write the number #_binaryNumber I must see the number #_decimalNumber as the result")
+  def "Convert a binary number to decimal"(){
+    when:
+      to ConverterPage
+    and: "I write a binary number"
+      number << _binaryNumber
+    and: "I press the calculate button"
+      calculate.click()
+      waitFor {result}
+    then: "I should see the decimal value"
+      assert result.text() == _decimalNumber
+    where:
+      _binaryNumber || _decimalNumber
+      "101"           ||  "5"
+      "111"           ||  "7"
+      "1000"          ||  "8"
+      "10001"         ||  "17"
+  }
 
   def "Conversion of a decimal number to binary"(){
     when:
@@ -26,7 +44,6 @@ class ConverterFunctionalSpec extends GebSpec {
     and: "I write a decimal number"
       number << "8"
     and: "I select the option to convert decimal to binary"
-      waitFor { typeOfConversion }
       typeOfConversion = 2
     and: "I press the calculate button"
       calculate.click()
@@ -34,5 +51,16 @@ class ConverterFunctionalSpec extends GebSpec {
     then: "I should see 1000 as the result"
       result.text() == "1000"
   }
-  
+
+  def "Conversion of a decimal number to binary"(){
+    when:
+      to ConverterPage
+    and: "I write a decimal number"
+      number << "25"
+    and: "I press the calculate button"
+      calculate.click()
+      waitFor { result }
+    then:"I should see an error message"
+      result.text() == "El número no puede ser convertido" 
+  }
 }
